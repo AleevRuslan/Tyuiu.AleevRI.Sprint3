40547a0b8c13534a27d2d61fe27bf8540b55ecb8
Tyuiu.AleevRI@outlook.com <aleevruslan3@gmail.com>
2023-10-22 18:00:19 +0500

    Task3

---
+++ Tyuiu.AleevRI.Sprint3.Task3.V8.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AleevRI.Sprint3.Task3.V8.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AleevRI.Sprint3.Task3.V8.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25816696-6540-4375-bfad-d0fa193b0dae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
