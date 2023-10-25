d527fee805cd8fdfa38725ca0eef169721afc34b
Tyuiu.AleevRI@outlook.com <aleevruslan3@gmail.com>
2023-10-25 14:15:04 +0500

    Task4

---
+++ Tyuiu.AleevRI.Sprint3.Task4.V14.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AleevRI.Sprint3.Task4.V14.Lib;
+
+namespace Tyuiu.AleevRI.Sprint3.Task4.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = 6.263;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.AleevRI.Sprint3.Task4.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AleevRI.Sprint3.Task4.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AleevRI.Sprint3.Task4.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5e1fbfb7-dc6a-4fce-8015-b762e922c5bc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
